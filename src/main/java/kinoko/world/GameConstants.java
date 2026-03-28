2c178ca873f22a515282d3e99de649ee877117b1
gmm4 <745165440@qq.com>
2026-03-29 01:06:42 +0800

    快捷键

--- src/main/java/kinoko/world/GameConstants.java
+++ src/main/java/kinoko/world/GameConstants.java
@@ -69,8 +69,11 @@ public final class GameConstants {
     public static final int MACRO_SKILL_COUNT = 3;
     public static final int FUNC_KEY_MAP_SIZE = 89;
     public static final FuncKeyMapped[] DEFAULT_FUNC_KEY_MAP = defaultFuncKeyMap(); // aDefaultFKM
-    public static final int QUICKSLOT_KEY_MAP_SIZE = 8;
-    public static final int[] DEFAULT_QUICKSLOT_KEY_MAP = new int[]{ 0x2A, 0x52, 0x47, 0x49, 0x1D, 0x53, 0x4F, 0x51 }; // aDefaultQKM
+    public static final int QUICKSLOT_KEY_MAP_SIZE = 62;
+    public static final int[] DEFAULT_QUICKSLOT_KEY_MAP = new int[]{
+            42, 82, 71, 73, 2, 3, 4, 5, 6, 30, 31, 32, 33, 34, 35, 7, 8, 9, 10, 11, 12, 13, 36, 37, 38, 59, 60, 61, 62, 63, 64,
+            29, 83, 79, 81, 16, 17, 18, 19, 20, 44, 45, 46, 47, 48, 49, 21, 22, 23, 24, 25, 26, 27, 50, 51, 52, 65, 66, 67, 68, 69, 70
+    }; // aDefaultQKM
 
     public static final int UNDEFINED_FIELD_ID = 999999999;
     public static final String DEFAULT_PORTAL_NAME = "sp"; // spawn point
